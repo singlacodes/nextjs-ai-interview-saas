bf795ed5aa6b8fc594c9446f658012d512b73c11
Shivam Singla <shivamsinglatechnical@gmail.com>
2026-07-08 01:15:15 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://nextjs-ai-interview-saas-1.onrender.com",
     credentials:true
 }))
 
